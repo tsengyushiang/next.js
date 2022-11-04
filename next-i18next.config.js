a3c95d5f08d227682b4c32152c0cc450cb45b4c3
yushiang <tsengyushiang@gmail.com>
2022-11-04 22:11:19 +0800

    feat : add next-i18next damo code

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'zh', 'zh-tw'], // folders in ./public/locales
+    },
+    interpolation: {
+        escapeValue: false,
+    },
+    lowerCaseLng: true, //support zh-tw
+};
